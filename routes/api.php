4a829c27c2c2942a39bd5619baff405dca957e1d
devjosiah11 <josiahkamau180@gmail.com>
2025-06-11 15:09:53 +0300

    Try deploy 1

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/products/{product}/approve', [ProductController::class, 'approve']);
     Route::get('/orders', [OrderController::class, 'index']);
     Route::post('/orders', [OrderController::class, 'store']);
+    Route::post('/order', [OrderController::class, 'store']);
+
     Route::post('/orders/{order}/status', [OrderController::class, 'updateStatus']);
 });
